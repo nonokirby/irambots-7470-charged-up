b04600a229e054b9d68ba7ac8b7a14972616084d
iRambots <82886780+nonokirby@users.noreply.github.com>
2023-02-27 18:29:34 -0500

    fixed clawgrab

--- src/main/java/frc/robot/commands/claw/clawGrabSquare.java
+++ src/main/java/frc/robot/commands/claw/clawGrabSquare.java
@@ -7,6 +7,7 @@ import frc.robot.RobotContainer;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.grabber;
+import frc.robot.Constants;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
