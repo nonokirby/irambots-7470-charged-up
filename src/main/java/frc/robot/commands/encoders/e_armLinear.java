9b7fb4e4258656c33343942a9ad27b6fb7a39e3a
nonokirby <82886780+nonokirby@users.noreply.github.com>
2023-03-02 14:48:21 +0000

    update e_armlin

--- src/main/java/frc/robot/commands/encoders/e_armLinear.java
+++ src/main/java/frc/robot/commands/encoders/e_armLinear.java
@@ -1,13 +1,13 @@
 package frc.robot.commands.encoders;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import frc.robot.subsystems.armDirectional;
+import frc.robot.subsystems.armLinear;
 
 public class e_armLinear extends InstantCommand {
     public e_armLinear(){
     }
     
     public void initialize(){
-        armDirectional.getLinEncoder();
+        armLinear.getLinEncoder();
     }
 }
