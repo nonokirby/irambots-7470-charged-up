94cffae47bedaf7456d830b6a0dfa9c492131a24
nonokirby <82886780+nonokirby@users.noreply.github.com>
2023-03-02 14:32:41 +0000

    add deadbands to drivemanual

--- src/main/java/frc/robot/commands/driveManual.java
+++ src/main/java/frc/robot/commands/driveManual.java
@@ -19,19 +19,19 @@ public class driveManual extends CommandBase {
     double rotation = RobotContainer.m_driver.getRawAxis(Constants.ax_driveTrainRotation);
     double swipeSpeed = RobotContainer.m_driver.getRawAxis(Constants.ax_driveTrainSwipe);
 
-    if(speed > -0.1 && speed < 0.1){
+    if(speed > -Constants.db_driveTrainSpeed && speed < Constants.db_driveTrainSpeed){
       speed = 0;
     }
 
-    if(rotation > -0.1 && rotation < 0.1){
+    if(rotation > -Constants.db_driveTrainRotationStill && rotation < Constants.db_driveTrainRotationStill){
       rotation = 0;
     }
-    if((speed < -0.1 || speed > 0.1) && (rotation < 0.2 && rotation > -0.2)){
+    if((speed < -Constants.db_driveTrainRotationSpeed || speed > Constants.db_driveTrainRotationSpeed) && (rotation < Constants.db_driveTrainRotationMove && rotation > -Constants.db_driveTrainRotationMove)){
       rotation = 0;
     }
     RobotContainer.driveTrain.driveCurvature(-speed * Constants.lm_driveTrainSpeed, rotation * Constants.lm_driveTrainRotation, true);
 
-     if (swipeSpeed <= 0.1 || swipeSpeed >= -0.1){
+     if (swipeSpeed <= Constants.db_driveTrainSwipe || swipeSpeed >= -Constants.db_driveTrainSwipe){
        RobotContainer.driveTrain.sideSwipe(swipeSpeed * Constants.lm_driveTrainSide);
      }
     }
